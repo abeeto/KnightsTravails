cf8383984f077353f2bf8df4fd9c31c36e9fb6e7
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-04-05 10:42:14 +0530

    Base UI and ESM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles.css";
+// import isInRange from "./utils";
 
-console.log("Hi! This is the entry point for the bundler");
+// const startPos = [1, 0];
+// eslint-disable-next-line no-console
+const positionForm = document.getElementById("knight-position-form");
+const submitButton = document.getElementById("knight-position-form-submit");
+const textOutputDiv = document.getElementById("knights-output-text");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const formData = new FormData(positionForm);
+
+  formData.entries().forEach(([key, value]) => {
+    textOutputDiv.innerText += `${key}: ${value} \n`;
+  });
+});
+
+const clearTextOutputBtn = document.querySelector("#clear-knights-output-btn");
+clearTextOutputBtn.addEventListener("click", () => {
+  textOutputDiv.innerText = "";
+});
